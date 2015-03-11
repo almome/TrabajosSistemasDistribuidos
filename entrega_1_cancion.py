edc2d2ccbc423d401a9843f3a4882c15fede7f43
mante-ko <alexandra.mome@gmail.com>
2015-03-12 00:06:19 +0100

    Cambio de nombre.

---
+++ entrega_1_cancion.py
@@ -0,0 +1,9 @@
+#GRUPO 4
+#Juan Manuel Hidalgo Navarro
+#Alexandra Moron Mendez
+
+#Programa que escribe cada verso de la cancion "99 botellas de ron".
+
+for botellas in range(99, 0, -1):
+    print botellas,"bottles of beer on the wall,", botellas,"bottles of beer.\n Take one down, pass it around,", botellas-1,"bottles of beer on the wall.\n"
+
